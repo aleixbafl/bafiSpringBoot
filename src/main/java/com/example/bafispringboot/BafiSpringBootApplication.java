08a961e06db2447539659e7912167d47392c43f8
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2024-09-25 13:35:01 +0200

    v0.1

---
+++ src/main/java/com/example/bafispringboot/BafiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bafispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BafiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BafiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/bafispringboot/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.example.bafispringboot.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainController {
+
+    @SpringBootApplication
+    public class Application {
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/minecraft")
+    public String minecraft(Model model) {
+        return "temes/minecraft";
+    }
+}
